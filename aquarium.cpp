0507c0112d52ebd62270a0debd784f3a88d37a60
Kysis101 <michenry@mymail.mines.edu>
2014-06-05 16:46:42 -0600

    Added a few methods to the aquarium.

--- aquarium.cpp
+++ aquarium.cpp
@@ -1,11 +1,23 @@
 #include "aquarium.h"
 
 
-Aquarium::Aquarium()
-{
+Aquarium::Aquarium(){
+	fish = new std::vector<Fish>();
+	temperature = 70;
 }
 
 
-Aquarium::~Aquarium()
-{
+Aquarium::~Aquarium(){
+}
+
+void Aquarium::addFish(Fish newFish){
+	fish->push_back(newFish);
+}
+
+int Aquarium::getTemperature(){
+	return temperature;
+}
+
+void Aquarium::setTemperature(int temp){
+	temperature = temp;
 }
--- aquarium.h
+++ aquarium.h
@@ -2,11 +2,16 @@
 #include <vector>
 #include "fish.h"
 class Aquarium{
+
 public:
 	Aquarium();
 	~Aquarium();
+	void addFish(Fish fish);
+	int getTemperature();
+	void setTemperature(int temp);
+
 private:
-	std::vector<Fish> fish;
+	std::vector<Fish>* fish;
 	int temperature;
 
 };
